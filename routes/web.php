489b83d2d1f4864ebd6f201e99261c357aad0128
matteoNapoli42 <matteon4242@gmail.com>
2023-11-09 16:00:53 +0100

    add dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +21,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+/*Questa route restituisce la vista alla dashboard solo agli utenti registrati, grazie al meccanismo del Middleware */
+
+
+/*Usiamo il middleware per raggruppare delle rotte. In questo caso appartengono alle pagine relative alla gestioen del profilo. Anche qui necessita della condizione di essere autenticato*/
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+//Include una serie di rotte per eseguire le operazioni degli utenti.
+require __DIR__ . '/auth.php';
