4244999fc62dcdf6b77a6168a76d5fd9ecc515e5
matteoNapoli42 <matteon4242@gmail.com>
2023-11-09 17:28:06 +0100

    create resource controller, workin on CRUD

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware('auth');
+
+
+
+
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
-
-
-/*Questa route restituisce la vista alla dashboard solo agli utenti registrati, grazie al meccanismo del Middleware */
-
 
 /*Usiamo il middleware per raggruppare delle rotte. In questo caso appartengono alle pagine relative alla gestioen del profilo. Anche qui necessita della condizione di essere autenticato*/
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
 //Include una serie di rotte per eseguire le operazioni degli utenti.
 require __DIR__ . '/auth.php';
